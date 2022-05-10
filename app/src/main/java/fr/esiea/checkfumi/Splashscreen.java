7168994f4eb7e2c48195b882e840ce1135093cec
Rym <djamouri-monno@et.esiea.fr>
2022-05-10 12:20:21 +0200

    splashscreen

--- app/src/main/java/fr/esiea/checkfumi/Splashscreen.java
+++ app/src/main/java/fr/esiea/checkfumi/Splashscreen.java
@@ -1,7 +1,9 @@
 package fr.esiea.checkfumi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        // rediriger vers la page principal après 3sec
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //Démarrer une page
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // handler post delayed
+        new Handler().postDelayed(runnable, 3000);
     }
 }
