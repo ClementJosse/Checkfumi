53c4fafebe4d9a253ceba3621b1fcc8047fac3a0
Rym <djamouri-monno@et.esiea.fr>
2022-06-13 21:42:42 +0200

    boutton tuto marche pas

--- app/src/main/java/fr/esiea/checkfumi/MainActivity.java
+++ app/src/main/java/fr/esiea/checkfumi/MainActivity.java
@@ -4,21 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView choix;
-    private ImageView choix2;
+    private Button choix2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.choix2 = (ImageView) findViewById(R.id.tuto);
         this.choix = (ImageView) findViewById(R.id.jouer);
-
+        this.choix2 = (Button) findViewById(R.id.button);
 
         choix.setOnClickListener(new View.OnClickListener() {
             @Override
